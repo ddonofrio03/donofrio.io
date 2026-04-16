bdbb53f0ae8cbb6930c34439d829472f12f0dbda
ddonofrio03 <ddonofrio@thecaseygroup.us>
2026-04-16 10:15:04 -0400

    Remove old static site file: config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.SITE_CONFIG = {
-  siteName: "DONOFR.IO",
-  siteTagline: "A family of builders",
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
-  adminEmail: "ddonofrio@thecaseygroup.us"
-};
